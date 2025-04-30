de04bc79f99cff369d0288b169ffc9eba2066465
romendezs <ro.mendez005@gmail.com>
2025-04-30 16:27:17 -0600

    feat: add api controllers

---
+++ NorthwindTraders.Api/Controllers/NorthwindController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthwindTraders.Application.Interfaces;
+
+namespace NorthwindTraders.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NorthwindController(INorthwindService service) : Controller
+    {
+        [HttpGet]
+        [Route("[action]ByDate/{date:DateTime}")]
+        public async Task<IActionResult> GetOrdersByDate(DateTime date)
+        {
+            var orders = await service.GetOrderByDate(date);
+            return Ok(orders);
+        }
+
+        [HttpGet]
+        [Route("[action]ByCustomer/{customerID}")]
+        public async Task<IActionResult> GetOrdersByCustomer(string customerID)
+        {
+            var orders = await service.GetOrderByCustomer(customerID);
+            return Ok(orders);
+        }
+
+        [HttpGet]
+        [Route("[action]ByEmployee/{employeeID}")]
+        public async Task<IActionResult> GetOrdersByEmployee(int employeeID)
+        {
+            var orders = await service.GetOrderByEmployee(employeeID);
+            return Ok(orders);
+        }
+
+        [HttpGet]
+        [Route("[action]/{orderID}")]
+        public async Task<IActionResult> GetOrdersByOrder(int orderID)
+        {
+            var orders = await service.GetOrderByOrder(orderID);
+            return Ok(orders);
+        }
+    }
+}
--- NorthwindTraders.Api/Program.cs
+++ NorthwindTraders.Api/Program.cs
@@ -1,3 +1,6 @@
+using NorthwindTraders.Application;
+using NorthwindTraders.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddAutoMapper(typeof(Program)); // Replace 'Program' with the assembly containing your AutoMapper profiles.
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
